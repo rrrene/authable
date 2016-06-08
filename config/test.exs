db63fea902feccce15b106976ff56191dc901766
Mustafa TURAN <mustafaturan.net@gmail.com>
2016-06-08 01:26:23 +0100

    Travis update for postgres.

--- config/test.exs
+++ config/test.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 config :authable, Authable.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "mustafaturan",
-  password: "postgres",
+  username: System.get_env["DATABASE_POSTGRESQL_USERNAME"] || "postgres",
+  password: System.get_env["DATABASE_POSTGRESQL_PASSWORD"] || "",
   database: "authable_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
